e0ac2889e95b3cc198c9097b914e945abe1d239b
Kevin Martensson <kevinmartensson@gmail.com>
2015-04-09 18:44:17 +0200

    Fix `sounds` not defined

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var unique = require('unique-random')(0, sounds.length - 1);
 var sounds = require('./sounds.json');
 
 module.exports = function () {
+	var unique = require('unique-random')(0, sounds.length - 1);
 	return sounds[unique()];
 };
 
